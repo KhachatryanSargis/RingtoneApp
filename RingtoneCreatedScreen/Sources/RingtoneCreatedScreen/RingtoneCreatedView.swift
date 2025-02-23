a518dde53c9ccf0e066d8f79c025179bf1435a18
Sargis Khachatryan <khachatrian.sargis@gmail.com>
2025-02-24 00:14:25 +0400

    added initial RingtoneCreatedViewModel

---
+++ RingtoneCreatedScreen/Sources/RingtoneCreatedScreen/RingtoneCreatedView.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  RingtoneCreatedScreen
+//
+//  Created by Sargis Khachatryan on 24.02.25.
+//
+
+import Foundation
---
+++ RingtoneKit/Sources/RingtoneKit/UILayer/Create/RingtoneCreatedViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  RingtoneCreatedViewModel.swift
+//  RingtoneKit
+//
+//  Created by Sargis Khachatryan on 24.02.25.
+//
+
+public final class RingtoneCreatedViewModel {
+    
+}
