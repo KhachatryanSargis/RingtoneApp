8a5e4aac2283b2199556b4de2874b02bd9327043
Sargis Khachatryan <khachatrian.sargis@gmail.com>
2025-02-24 00:18:58 +0400

    added RingtoneCreatedViewModelFactory

--- RingtoneKit/Sources/RingtoneKit/UILayer/Create/RingtoneCreatedViewModel.swift
+++ RingtoneKit/Sources/RingtoneKit/UILayer/Create/RingtoneCreatedViewModel.swift
@@ -5,6 +5,10 @@
 //  Created by Sargis Khachatryan on 24.02.25.
 //
 
+public protocol RingtoneCreatedViewModelFactory {
+    func makeRingtoneCreatedViewModel() -> RingtoneCreatedViewModel
+}
+
 public final class RingtoneCreatedViewModel {
     
 }
