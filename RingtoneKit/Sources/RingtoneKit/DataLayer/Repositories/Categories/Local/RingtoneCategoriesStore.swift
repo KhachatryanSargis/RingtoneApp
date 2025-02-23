08084981534032dc41449bfe3e72210be5c9f9bf
Sargis Khachatryan <khachatrian.sargis@gmail.com>
2025-02-24 00:30:07 +0400

    fixed a typo

--- RingtoneKit/Sources/RingtoneKit/DataLayer/Repositories/Categories/Local/RingtoneCategoriesStore.swift
+++ RingtoneKit/Sources/RingtoneKit/DataLayer/Repositories/Categories/Local/RingtoneCategoriesStore.swift
@@ -19,7 +19,7 @@ public final class RingtoneCategoriesStore: IRingtoneCategoriesStore {
 
 fileprivate let categories: [RingtoneCategory] = [
     .init(displayName: "Trending", folderName: "", color: .init(lightHex: "bde0fe", darkHex: "023e8a")),
-    .init(displayName: "Hip-Hop", folderName: "", color: .init(lightHex: "ccd5ae", darkHex: "386641")),
+    .init(displayName: "Hip Hop", folderName: "", color: .init(lightHex: "ccd5ae", darkHex: "386641")),
     .init(displayName: "Jazz", folderName: "", color: .init(lightHex: "83c5be", darkHex: "006d77")),
     .init(displayName: "Electro", folderName: "", color: .init(lightHex: "e0aaff", darkHex: "451f55")),
     .init(displayName: "Prank", folderName: "", color: .init(lightHex: "ffa5ab", darkHex: "9e2a2b")),
