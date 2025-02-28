0b6129d49a637de32b0b6ffd1f6a7ed981795276
Sargis Khachatryan <khachatrian.sargis@gmail.com>
2025-02-28 17:20:59 +0400

    add new properties to RingtoneAudio model

--- RingtoneKit/Sources/RingtoneKit/DataLayer/Model/Audio/RingtoneAudio.swift
+++ RingtoneKit/Sources/RingtoneKit/DataLayer/Model/Audio/RingtoneAudio.swift
@@ -5,18 +5,42 @@
 //  Created by Sargis Khachatryan on 26.02.25.
 //
 
-public struct RingtoneAudio: Equatable, Hashable, Sendable {
+import Foundation
+
+public struct RingtoneAudio: Identifiable, Equatable, Hashable, Sendable {
+    public var id: String {
+        return title
+    }
     public let title: String
     public let categoryID: String
     public let isCreated: Bool
+    public let isPlaying: Bool
+    public let isLiked: Bool
     
     public init(
         title: String,
         categoryID: String = "Other",
-        isCreated: Bool = false
+        isCreated: Bool = false,
+        isPlaying: Bool = false,
+        isLiked: Bool = false
     ) {
         self.title = title
         self.categoryID = categoryID
         self.isCreated = isCreated
+        self.isPlaying = isPlaying
+        self.isLiked = isLiked
+    }
+}
+
+// MARK: - Like, Unlike
+extension RingtoneAudio {
+    public func likeToggled() -> RingtoneAudio {
+        RingtoneAudio(
+            title: self.title,
+            categoryID: self.categoryID,
+            isCreated: self.isCreated,
+            isPlaying: self.isPlaying,
+            isLiked: self.isLiked ? false : true
+        )
     }
 }
