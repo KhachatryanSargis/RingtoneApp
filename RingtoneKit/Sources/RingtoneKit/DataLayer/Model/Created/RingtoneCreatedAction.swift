87a1a8cc9e66d58a5dc4264ed1943284b5885a41
Sargis Khachatryan <khachatrian.sargis@gmail.com>
2025-03-03 21:39:34 +0400

    added RingtoneCreatedAction

--- RingtoneKit/Sources/RingtoneKit/DataLayer/Model/Created/RingtoneCreatedAction.swift
+++ RingtoneKit/Sources/RingtoneKit/DataLayer/Model/Created/RingtoneCreatedAction.swift
@@ -1,3 +1,10 @@
+//
+//  RingtoneCreatedAction.swift
+//  RingtoneKit
+//
+//  Created by Sargis Khachatryan on 03.03.25.
+//
+
 public enum RingtoneCreatedAction {
     case importAudio
     case export(_ audio: RingtoneAudio)
