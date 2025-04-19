b9cb461cc893a78ec8b939a65090f01117099e68
Sargis Khachatryan <khachatrian.sargis@gmail.com>
2025-04-19 22:04:08 +0400

    added cases to RingtoneDataConverterError and storage error checks

--- RingtoneKit/Sources/RingtoneKit/DataLayer/Model/DataConverter/RingtoneDataConverterError.swift
+++ RingtoneKit/Sources/RingtoneKit/DataLayer/Model/DataConverter/RingtoneDataConverterError.swift
@@ -5,12 +5,34 @@
 //  Created by Sargis Khachatryan on 04.03.25.
 //
 
+import Foundation
+
 public enum RingtoneDataConverterError: Error, Sendable {
     case failedToCreateReader(Error)
     case failedToAddReaderOutput
     case failedToCreateWriter(Error)
     case failedToAddWriterInput
-    case exportSessionError(Error)
     case loadAudioTrackError(Error)
+    case reader(Error)
+    case writer(Error)
+    case failedToSaveWaveform(Error)
     case unexpected
+    
+    var isStorageFull: Bool {
+        if case .writer(let error) = self {
+            return (error as NSError).code == NSFileWriteOutOfSpaceError
+        } else if case .failedToSaveWaveform(let error) = self {
+            return (error as NSError).code == NSFileWriteOutOfSpaceError
+        } else {
+            return false
+        }
+    }
+    
+    var message: String {
+        if isStorageFull {
+            return "Your iPhone storage is full."
+        } else {
+            return "\(self)"
+        }
+    }
 }
