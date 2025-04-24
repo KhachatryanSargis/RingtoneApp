4a7fb787885cda35b38527fbc20f62c079a1084b
Sargis Khachatryan <khachatrian.sargis@gmail.com>
2025-04-24 18:47:57 +0400

    cleanup

--- RingtoneKit/Sources/RingtoneKit/DataLayer/Services/DataEditor/Operations/TrimAudioOperation.swift
+++ RingtoneKit/Sources/RingtoneKit/DataLayer/Services/DataEditor/Operations/TrimAudioOperation.swift
@@ -442,37 +442,3 @@ final class TrimAudioOperation: AsyncOperation, @unchecked Sendable {
         self.state = .finished
     }
 }
-
-// MARK: - File Size / Formatted File Size
-extension URL {
-    func getFileSize() -> Double? {
-        do {
-            let fileAttributes = try FileManager.default.attributesOfItem(atPath: path)
-            if let fileSize = fileAttributes[.size] as? NSNumber {
-                return fileSize.doubleValue
-            } else {
-                return nil
-            }
-        } catch {
-            return nil
-        }
-    }
-    
-    func getFormattedFileSize() -> String {
-        guard let fileSize = getFileSize()
-        else { return "Unknown Size" }
-        
-        let fileSizeInMB = fileSize / (1024 * 1024)
-        
-        let formattedFileSize: String
-        
-        if fileSizeInMB < 1 {
-            let fileSizeInKB = fileSize / 1024
-            formattedFileSize = String(format: "%.0f KB", fileSizeInKB)
-        } else {
-            formattedFileSize = String(format: "%.1f MB", fileSizeInMB)
-        }
-        
-        return formattedFileSize
-    }
-}
