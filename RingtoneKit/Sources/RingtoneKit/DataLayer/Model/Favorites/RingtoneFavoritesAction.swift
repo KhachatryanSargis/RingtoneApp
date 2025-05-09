fd127160b6ad7a42fd3a4331dffcd4baa3092215
Sargis Khachatryan <khachatrian.sargis@gmail.com>
2025-05-09 13:52:21 +0400

    added missing action cases

--- RingtoneKit/Sources/RingtoneKit/DataLayer/Model/Favorites/RingtoneFavoritesAction.swift
+++ RingtoneKit/Sources/RingtoneKit/DataLayer/Model/Favorites/RingtoneFavoritesAction.swift
@@ -1,5 +1,5 @@
 //
-//  File.swift
+//  RingtoneFavoritesAction.swift
 //  RingtoneFavoritesAction
 //
 //  Created by Sargis Khachatryan on 06.04.25.
@@ -8,6 +8,8 @@
 import Foundation
 
 public enum RingtoneFavoritesAction {
-    case exportGarageBandProjects(_ urls: [URL])
     case editAudio(_ audio: RingtoneAudio)
+    case exportGarageBandProject(_ url: URL, _ audio: RingtoneAudio)
+    case exportAudios(_ audios: [RingtoneAudio])
+    case showUsageTutorial
 }
