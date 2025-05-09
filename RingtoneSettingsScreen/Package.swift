161de8421b0805b92b7c3f284607099ae9f7a2ab
Sargis Khachatryan <khachatrian.sargis@gmail.com>
2025-05-09 14:27:13 +0400

    added empty RingtoneSettingsScreen package

---
+++ RingtoneSettingsScreen/Package.swift
@@ -0,0 +1,28 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "RingtoneSettingsScreen",
+    platforms: [.iOS(.v15)],
+    products: [
+        .library(
+            name: "RingtoneSettingsScreen",
+            targets: ["RingtoneSettingsScreen"]
+        ),
+    ],
+    dependencies: [
+        .package(path: "RingtoneUIKit"),
+        .package(path: "RingtoneKit")
+    ],
+    targets: [
+        .target(
+            name: "RingtoneSettingsScreen",
+            dependencies: [
+                "RingtoneUIKit",
+                "RingtoneKit"
+            ]
+        ),
+    ]
+)
