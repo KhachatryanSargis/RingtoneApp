afb00b71c014329ba09efcc03c80f849f63eba36
Sargis Khachatryan <khachatrian.sargis@gmail.com>
2025-05-10 14:04:38 +0400

    added view model injection

--- RingtoneSettingsScreen/Sources/RingtoneSettingsScreen/RingtoneSettingsView.swift
+++ RingtoneSettingsScreen/Sources/RingtoneSettingsScreen/RingtoneSettingsView.swift
@@ -6,7 +6,15 @@
 //
 
 import RingtoneUIKit
+import RingtoneKit
 
 final class RingtoneSettingsView: NiblessView {
+    // MARK: - Properties
+    private let viewModel: RingtoneSettingsViewModel
     
+    // MARK: - Methods
+    init(viewModel: RingtoneSettingsViewModel) {
+        self.viewModel = viewModel
+        super.init()
+    }
 }
